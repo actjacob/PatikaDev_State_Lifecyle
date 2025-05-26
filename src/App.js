829914613ec0ee179428c50584e794bb8e429940
actjacob <yakupaktar99@gmail.com>
2025-05-26 12:57:13 +0300

    State konusuna giriş yapıldı ve useStatein kullanım alanları ve nasıl kullanıldığı örnekleri eklendi

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  function increaseCounter() {
+    setCounter(counter + 1);
+  }
+
+  function decreaseCounter() {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 40}}>Counter:{counter}</Text>
+      <Button title="increase Counter" onPress={increaseCounter} />
+      <Button title="Decrease Counter" onPress={decreaseCounter} />
+    </SafeAreaView>
+  );
+};
+export default App;
